b4dbf0baec370dad20e003c3311c58303f57161e
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-07-01 20:47:20 -0400

    Pattern.java

--- Programming Exercise 21.3 /Pattern.java
+++ Programming Exercise 21.3 /Pattern.java
@@ -1,16 +1,15 @@
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-
+// Demonstrates pattern matching in switch statements (Java 17+)
 public class Pattern {
   public static void process(Object obj) {
     switch (obj) {
       case String s -> System.out.println("String: " + s.toUpperCase());
       case Integer i -> System.out.println("Integer squared: " + (i * i));
       case Double d -> System.out.println("Double value: " + (d * 2));
-      case null -> System.out.println("Null value recieved");
+      case null -> System.out.println("Null value received");
       default -> System.out.println("Unknown type: " + obj);
     }
   }
+
   public static void main(String[] args) {
     process("Animal");
     process(4);
